b65c55ea5cb5423c162539ad5615296c0ab06382
Krish Sirsath <krishsirsath21@gnu.ac.in>
2026-01-16 23:29:54 +0530

    Frontend setup done

---
+++ Frontend/sunocampus/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Feed from "./pages/Feed";
+import Events from "./pages/Events";
+import Profile from "./pages/Profile";
+import Admin from "./pages/Admin";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Feed />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Frontend/sunocampus/src/pages/Login.jsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <h1>Campus Login Page</h1>;
+}
